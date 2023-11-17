adb6b04829cd1c3cb96c7007bd19e6f2ce387e21
Sho0ting-StaR <146768151+Sho0ting-StaR@users.noreply.github.com>
2023-11-17 12:17:13 -0600

    starting grid mapping

--- sketch.js
+++ sketch.js
@@ -6,11 +6,42 @@
 // - describe what you did to take this project "above and beyond"
 
 
+let grid = [];
+let tilesize = 72;
+let scale = height/tilesize;
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  createGrid(,72,36);
 }
 
 function draw() {
   background(220);
   circle(mouseX,mouseY,50);
+}
+
+function displayGrid(){
+  for(let n of grid){
+    for(let m of grid[n]){
+      rect(m*scale,n*scale,scale,scale);
+    }
+  }
+}
+
+function createGrid(scaling,numx,numy) {
+  for(let n of numy){
+    grid.push([]);
+    for(let m of numx){
+      grid[n].push(tileGenerate());
+      
+    }
+  }
+}
+
+function tileGenerate() {
+  let array = [];
+  //array.push(Math.floor(random(0,4))); //terrain type (grass,water,rock,tree)
+  array.push(Math.floor(random(0,4))); //biome type (4 of savannah,desert,tundra,wastes,plains,forest,swamp)
+  array.push(0); //territory (unclaimed only)
+  //array.push(Math.floor(random(0,7))); // density for better generation
+  return array;
 }
