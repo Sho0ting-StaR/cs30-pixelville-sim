d99670a700cbf86c10be6c8bcbe39babdc1b6739
Sho0ting-StaR <146768151+Sho0ting-StaR@users.noreply.github.com>
2023-11-20 12:19:01 -0600

    foundations of map generation pixelville
    started perlin noise for better terrain generation.
    
    first version of grid display,
    
    first colouring(no biomes)

--- sketch.js
+++ sketch.js
@@ -8,40 +8,77 @@
 
 let grid = [];
 let tilesize = 72;
-let scale = height/tilesize;
+let wScale;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  createGrid(,72,36);
+  wScale = windowHeight/(tilesize/2);
+  createGrid(72,36);
 }
 
 function draw() {
-  background(220);
-  circle(mouseX,mouseY,50);
+  // circle(mouseX,mouseY,50);
+  displayGrid();
 }
 
+
+
 function displayGrid(){
-  for(let n of grid){
-    for(let m of grid[n]){
-      rect(m*scale,n*scale,scale,scale);
+  let r;
+  let g;
+  let b;
+  for(let n =0; n< grid.length;n++){
+    for(let m =0;m < grid[n].length;m++){
+      noStroke();
+      fill(190);
+      text(grid[n][m][0],m*tilesize,n*tilesize); // terrain type
+      if(grid[n][m][0] === 0){ //water // drawing correct terrain
+        r = 40; 
+        g = 80; 
+        b= random(100,131);
+      }
+      else if(grid[n][m][0] === 1){ // grass
+        r = 40; 
+        g = 80; 
+        b = 30;
+      }
+      else if(grid[n][m][0] === 2){ // forest
+        r =0; 
+        g =35; 
+        b =0;
+      }
+      else if(grid[n][m][0] === 3){ //rock
+        r = 135; 
+        g = 135; 
+        b = 135;
+      }
+      fill(r,g,b);
+      rect(m*tilesize,n*tilesize,tilesize,tilesize);
+      // console.log(grid[n][m][2]); density
     }
   }
 }
 
-function createGrid(scaling,numx,numy) {
-  for(let n of numy){
+function createGrid(numx,numy) {
+  for(let n =0;n<numy;n++){
     grid.push([]);
-    for(let m of numx){
-      grid[n].push(tileGenerate());
+    for(let m =0;m<numx;m++){
+      grid[n].push(tileGenerate(m));
+      // console.log(grid[n]);
       
     }
   }
 }
 
-function tileGenerate() {
+function tileGenerate(m) {
   let array = [];
   //array.push(Math.floor(random(0,4))); //terrain type (grass,water,rock,tree)
-  array.push(Math.floor(random(0,4))); //biome type (4 of savannah,desert,tundra,wastes,plains,forest,swamp)
+  // let n2 = n*4;
+  // let m2 = m*4;
+  // n2 = noise(,);
+  array.push(Math.floor(noise(m)*4));
+  // array.push(Math.floor(random(0,4))); //biome type (4 of savannah,desert,tundra,wastes,plains,forest,swamp)
   array.push(0); //territory (unclaimed only)
-  //array.push(Math.floor(random(0,7))); // density for better generation
+  array.push(Math.floor(random(0,7))); // density for better generation
+  // console.log(array);
   return array;
 }
